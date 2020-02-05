cafa4ced3d6e27215bab766580ff882c0e10cb14
Shashi Bhushan <shashi.sastra1@gmail.com>
2020-02-05 00:33:48 -0500

    Testing commit message check again

--- .githooks/commit-message-check.sh
+++ .githooks/commit-message-check.sh
@@ -6,7 +6,7 @@ commit_message_check (){
       currentbranch=`git branch | grep \* | cut -d ' ' -f2`
 
       # Gets the commits for the current branch and outputs to file
-      git log $currentbranch --pretty=format:"%H" --not master > shafile.txt
+      git log $currentbranch --pretty=format:"%H" > shafile.txt
 
       # loops through the file an gets the message
       for i in `cat ./shafile.txt`;
