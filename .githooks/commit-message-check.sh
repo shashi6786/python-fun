239f2e960698b1b85cf05715f4e097b4c1b971d3
Shashi Bhushan <shashi.sastra1@gmail.com>
2020-02-05 01:32:48 -0500

    US12344 Testing commit message check again - 13

--- .githooks/commit-message-check.sh
+++ .githooks/commit-message-check.sh
@@ -3,13 +3,13 @@
     echo " Entering checker......."
       # Get the current branch and apply it to a variable
       currentbranch=`git branch | grep \* | cut -d ' ' -f2`
-      
+
       # Gets the commits for the current branch and outputs to file
       #git log $currentbranch --pretty=format:"%H" > shafile.txt
 
       # gets the git commit message based on the sha
       #gitmessage=`git log --format=%B -n 1 "$i"`
-      gitmessage=$1
+      gitmessage="$1"
       echo "commit text is : " $gitmessage
       # Checks gitmessage for string feat, fix, docs and breaking, if the messagecheck var is empty if fails
       messagecheck=`echo $gitmessage | grep "^US[0-9]\{5\}"`
